442dedb34acb7f95970b01861bfcf8d97f216852
Priyanshu Joshi <joshipriyanshu575@gmail.com>
2026-06-19 23:01:38 +0530

    frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ✅ Required for static export on Render
-  output: 'export',
-  
-  // ✅ Required for static export (disables image optimization)
+
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
-  
-  // ✅ Makes API URL available in browser
   env: {
     NEXT_PUBLIC_API_URL: 'https://demotrust.onrender.com',
-  }
-}
+  },
+};
+
 export default nextConfig;
